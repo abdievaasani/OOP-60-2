8284fe2b8c3fbeea4fb19e87937ec1d7d49dacf1
abdievaasani <abdievaasani@gmail.com>
2025-11-21 11:56:58 +0600

    new

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+import match_module as m
+from match_module import (
+    add, mul, count, random
+)
+
+print(add(1,2))
+print(mul(2,3))
+print(count)
+print(random.randint(1,3))
---
+++ match_module.py
@@ -0,0 +1,8 @@
+import random
+count =  234
+
+def add(a, b):
+    return a+b
+
+def mul(a, b):
+    return a*b
